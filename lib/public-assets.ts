bb29538776a0fd6c365f988e2d6f411c00d18c4e
emremutlu08 <mutluemre93@gmail.com>
2026-05-04 19:52:30 +0300

    Update founder image to city portrait

--- lib/public-assets.ts
+++ lib/public-assets.ts
@@ -9,7 +9,7 @@ export function hasPublicAsset(assetPath: string) {
   return existsSync(resolvePublicAsset(assetPath));
 }
 
-export const FOUNDER_PHOTO_PATH = "/emre-mutlu.webp";
+export const FOUNDER_PHOTO_PATH = "/emre-city-16x9.png";
 export const UPWORK_BADGE_PATH = "/badges/upwork-top-rated-plus.svg";
 
 export const HAS_FOUNDER_PHOTO = hasPublicAsset(FOUNDER_PHOTO_PATH);
